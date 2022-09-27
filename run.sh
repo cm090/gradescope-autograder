7affa84bf8db0499563e5348994a361c29454254
Canon Maranda <22649817+cm090@users.noreply.github.com>
2022-09-27 15:14:25 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# On line 6, change [project] to the project RunAllTests.java is part of
-# Add test files to the src directory
-# Zip files (not the containing folder) and upload to assignment
-javac -cp src/:lib/* -d bin/ src/[project]/RunAllTests.java
+javac -cp src/:lib/* -d bin/ src/[HWName]/RunAllTests.java
 java -cp bin/:lib/* AutoGrader.GradescopeAutoGrader
 cp results.json /autograder/results
