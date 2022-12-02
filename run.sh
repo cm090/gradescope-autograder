1d02f510f249dd49a55cf589f6a688dfb91b9950
Canon Maranda <22649817+cm090@users.noreply.github.com>
2022-12-02 13:33:59 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,5 +12,5 @@ java -cp bin/:lib/* AutoGrader.GradescopeAutoGrader $SCORE $TESTS
 cp results.json /autograder/results
 else
 echo "There was a problem compiling the student's submission"
-cat $OUT
+echo java.out
 fi
