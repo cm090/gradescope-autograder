a989afe8746526bf359d387b606acfb7a7e72c46
Canon Maranda <22649817+cm090@users.noreply.github.com>
2025-01-10 12:55:50 -0500

    Extract methods in results

--- templates/homework/src/newAutograder/Results.java
+++ templates/homework/src/newAutograder/Results.java
@@ -13,11 +13,15 @@ public class Results {
     private Map<String, Double> testWeights;
     private Map<String, Integer> testCounts;
     private OutputMessage outputMessage;
+    private boolean bypassScoreCalculation;
+    private double totalScore;
 
     private Results() {
         testResults = new HashMap<>();
         testCounts = new HashMap<>();
         outputMessage = OutputMessage.DEFAULT;
+        bypassScoreCalculation = false;
+        totalScore = 0.0;
     }
 
     void addTest(String name, Visibility visibility) {
@@ -47,47 +51,55 @@ public class Results {
 
     void toJson(double percentage) {
         percentage /= 100.0;
-        double totalScore = 0.0;
-        boolean bypassScoreCalculation = false;
         JSONObject json = new JSONObject();
         JSONArray tests = new JSONArray();
 
-        for (Entry<String, TestData> entry : testResults.entrySet()) {
-            JSONObject test = new JSONObject();
-            test.put("score", entry.getValue().getScore());
-            test.put("max_score", entry.getValue().getMaxScore());
-            test.put("name", entry.getValue().getName());
-            test.put("number", entry.getKey().toString());
-            test.put("output", entry.getValue().getOutputText().replaceAll("\t", " "));
-            test.put("visibility", entry.getValue().getVisibility().getValue());
-            if (entry.getValue().getOutputText().length() > 0) {
-                test.put("status", "failed");
-            }
-            tests.put(test);
+        testResults.entrySet().forEach((entry) -> {
+            buildTestResultObject(entry, tests);
+            checkAlternateScoreCalculation(entry);
+        });
 
-            if (!bypassScoreCalculation) {
-                // Calculate score based on test weight
-                String currentName =
-                        entry.getValue().getName().indexOf(".") == -1 ? entry.getValue().getName()
-                                : entry.getValue().getName().substring(0,
-                                        entry.getValue().getName().lastIndexOf("."));
-                double currentWeight = testWeights.get(currentName);
-                if (currentWeight < 0) {
-                    // Calculate score based on number of tests
-                    bypassScoreCalculation = true;
-                }
-                totalScore += (entry.getValue().getScore() * currentWeight)
-                        / testCounts.getOrDefault(currentName, 1);
-            }
+        writeGlobalResults(json, tests, percentage);
+        Configuration.instance.writeToOutput(json);
+    }
+
+    private void buildTestResultObject(Entry<String, TestData> entry, JSONArray tests) {
+        JSONObject test = new JSONObject();
+        test.put("score", entry.getValue().getScore());
+        test.put("max_score", entry.getValue().getMaxScore());
+        test.put("name", entry.getValue().getName());
+        test.put("number", entry.getKey().toString());
+        test.put("output", entry.getValue().getOutputText().replaceAll("\t", " "));
+        test.put("visibility", entry.getValue().getVisibility().getValue());
+        if (entry.getValue().getOutputText().length() > 0) {
+            test.put("status", "failed");
         }
+        tests.put(test);
+    }
 
+    private void checkAlternateScoreCalculation(Entry<String, TestData> entry) {
+        if (!bypassScoreCalculation) {
+            // Calculate score based on test weight
+            String currentName =
+                    entry.getValue().getName().indexOf(".") == -1 ? entry.getValue().getName()
+                            : entry.getValue().getName().substring(0,
+                                    entry.getValue().getName().lastIndexOf("."));
+            double currentWeight = testWeights.get(currentName);
+            if (currentWeight < 0) {
+                // Calculate score based on number of tests
+                bypassScoreCalculation = true;
+            }
+            totalScore += (entry.getValue().getScore() * currentWeight)
+                    / testCounts.getOrDefault(currentName, 1);
+        }
+    }
+
+    private void writeGlobalResults(JSONObject json, JSONArray tests, double percentage) {
         json.put("score", bypassScoreCalculation ? percentage * Configuration.instance.getMaxScore()
                 : totalScore);
         json.put("output", outputMessage.getValue());
         json.put("output_format", "md");
         json.put("visibility", "visible");
         json.put("tests", tests);
-
-        Configuration.instance.writeToOutput(json);
     }
 }
